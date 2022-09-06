247686fa825cf75afe44cd4017b79896e878a278
Asad Sheikh <sheikhasad22574@gmail.com>
2022-09-06 12:15:51 +0500

    Initial commit

--- lib/screens/wishlist_screen.dart
+++ lib/screens/wishlist_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/models/product.dart';
+import 'package:shop_app/widgets/product_card.dart';
 
 class WishlistScreen extends StatelessWidget {
   const WishlistScreen({super.key});
@@ -6,6 +8,20 @@ class WishlistScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1.15,
+      ),
+      itemBuilder: (context, index) {
+        return Center(
+          child: ProductCard(
+            product: Product.products[index],
+            widthFactor: 2.2,
+          ),
+        );
+      },
+      itemCount: Product.products.length,
+    );
   }
 }
